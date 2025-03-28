f8fc5dfce91f80421217d73e3e1d9b0fc4c0948e
Mosarrof-Coding <tinaberry112188@gmail.com>
2025-03-28 23:28:56 +0600

    Add:toggler

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,11 +1,12 @@
 "use client";
-import { ChevronDown, MoonIcon, SunIcon } from "lucide-react";
+import { AlignJustify, ChevronDown, MoonIcon, SunIcon } from "lucide-react";
 import { useTheme } from "next-themes";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 export default function Navbar() {
   const { theme, setTheme } = useTheme();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const navItems = [
     { name: "Features", href: "/features" },
@@ -28,14 +29,14 @@ export default function Navbar() {
         <nav className="navmain flex items-center justify-between">
           <Link
             href="/"
-            className="logo text-2xl lg:text-3xl font-bold text-[#5BB5A2] dark:text-[#d2fff5]"
+            className="logo text-2xl lg:text-3xl font-bold text-[#5BB5A2] dark:text-[#d2fff5] mr-4"
           >
             banquee
           </Link>
           <div className="hidden lg:flex gap-3 lg:gap-6">
             {navItems.map((item, i) => (
               <div
-                className="flex items-center gap-2 sm:gap-3 lg:gap-4"
+                className="flex items-center gap-1 sm:gap-2 lg:gap-4"
                 key={i}
               >
                 <Link href={item.href} className="navlist">
@@ -45,7 +46,7 @@ export default function Navbar() {
               </div>
             ))}
           </div>
-          <div className="flex items-center gap-2 lg:gap-4">
+          <div className="hidden lg:flex items-center gap-2 lg:gap-4">
             <div className="resister flex items-center gap-2 lg:gap-4">
               <div
                 className="dark-light cursor-pointer w-6 lg:w-7 aspect-square grid place-items-center rounded-full"
@@ -68,6 +69,53 @@ export default function Navbar() {
                 Open Account
               </Link>
             </div>
+          </div>
+
+          {/* mobilemenu */}
+          <div className="mobilemenu flex items-center gap-2 lg:hidden">
+            <div
+              className="dark-light cursor-pointer w-6 lg:w-7 aspect-square grid place-items-center rounded-full"
+              onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+            >
+              {theme === "light" ? (
+                <MoonIcon className="w-4 lg:w-5" />
+              ) : (
+                <SunIcon className="w-4 lg:w-5" color="#fff" />
+              )}
+            </div>
+
+            {/* Menu Toggle Button */}
+            <div
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+              className="cursor-pointer"
+            >
+              <AlignJustify strokeWidth={1.75} absoluteStrokeWidth />
+            </div>
+
+            {/* Mobile Menu Panel */}
+            {isMenuOpen && (
+              <div className="fixed left-0 top-0 right-0 h-screen bg-white z-50">
+                <button
+                  className="w-full py-2 sm:py-3 bg-[#1a191e] text-white text-left text-xl sm:text-2xl pl-8 cursor-pointer"
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  Back
+                </button>
+                <div className="flex flex-col">
+                  {navItems.map((item, i) => (
+                    <Link
+                      key={i}
+                      href={item.href}
+                      className="text-sm sm:text-[16px] py-2 hover:bg-gray-600 text-[#1a191e] hover:text-white flex items-center transition-all pl-12"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      {item.name}
+                      {item.icon && item.icon}
+                    </Link>
+                  ))}
+                </div>
+              </div>
+            )}
           </div>
         </nav>
       </div>
